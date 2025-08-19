81da4980a73eddb04dd1ab563e2677ab5173b280
Little Pooh <77505794+Little-Pooh0@users.noreply.github.com>
2025-08-19 16:20:17 -0500

    Update MCP.cpp
    Use Plugin name for main MCP category.

--- src/MCP.cpp
+++ src/MCP.cpp
@@ -7,7 +7,7 @@ namespace MCP {
 			return;
 			logger::error("SKSEMenuFramework is not installed. MCP functionality cannot be registered.");
 		}
-		SKSEMenuFramework::SetSection("Better Jumping SE");
+		SKSEMenuFramework::SetSection("{}", SKSE::PluginDeclaration::GetSingleton()->GetName());
 		SKSEMenuFramework::AddSectionItem("General Settings", General::Render);
 	}
 
