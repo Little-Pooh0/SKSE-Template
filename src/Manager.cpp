8c79f716cde278afe556027c8f2c217833ca9aa9
Little Pooh <77505794+Little-Pooh0@users.noreply.github.com>
2025-08-19 16:41:19 -0500

    Update Manager.cpp
    Use Plugin name instead of Static Name for Logging.

--- src/Manager.cpp
+++ src/Manager.cpp
@@ -6,12 +6,12 @@ namespace Manager {
 	* Called from the SKSE plugin entry point after game data is loaded.
 	*/
 	void Initialize() {
-		logger::info("Manager: JumpingMod Initializing, Entering and Allocating trampoline.");
+		logger::info("Manager: {} Initializing, Entering and Allocating trampoline.", SKSE::PluginDeclaration::GetSingleton()->GetName());
 
 		// Allocate trampoline space early. Commonlib handles getting the SKSE trampoline.
 		// Requesting reasonable sizes for branch and local (code generation) trampolines.
 		// Combined size might need adjustment based on total hooks from all mods.
-		SKSE::AllocTrampoline(1 << 10);  // 2^10 = 1024 bytes for branch trampoline
+		SKSE::AllocTrampoline(14);  // Only 14 bytes are necessary. SKSE will automatically allocate the correct amount.
 		logger::info("Manager: Trampoline allocated, now Calling ResolveAddresses");
 
 		// Resolve necessary game addresses using Address Library.
@@ -20,7 +20,7 @@ namespace Manager {
 		// Install memory hooks and patches.
 		Hooks::InstallHooks();
 		
-		logger::info("Manager: Exiting, InstallHooks finished and jumpingmod Initialization finished.");
+		logger::info("Manager: Exiting, InstallHooks finished and {} Initialization finished.", SKSE::PluginDeclaration::GetSingleton()->GetName());
 		return;
 	}
 
